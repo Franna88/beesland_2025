62a82ee21088e7b0c752d70f3d935977dd16a180
JacoHg <168348167+JacoHg@users.noreply.github.com>
2025-07-22 15:28:24 +0200

    image update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -380,7 +380,7 @@ class HomeScreen extends StatelessWidget {
                                 ],
                                 image: const DecorationImage(
                                   image: NetworkImage(
-                                    'https://images.unsplash.com/photo-1547637589-f54c34f5d7a4?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+                                    'https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                                   ),
                                   fit: BoxFit.cover,
                                 ),
